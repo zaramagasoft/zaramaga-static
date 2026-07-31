c13be3f3ccc9949a1ce3f065b66842ad4f241988
Zaramagasoft <zaramagasoft@gmail.com>
2026-07-31 16:29:00 +0200

    cairo minimo para zsplash ya compilado

--- projects/zlauncher/notas.md
+++ projects/zlauncher/notas.md
@@ -1,3 +1,16 @@
+$ZSYS/bin/musl-gcc pruebas.c -static -I$ZSYS/include -
+I$ZSYS/include/cairo -L$ZSYS/lib -lcairo -l:libpng18.a -lfreetype -lpixman-1 -ldrm
+ -lz -lm -lpthread -o pruebas
+[alb@zaramagaOS zlauncher]$ ./pruebas 
+SDK Zaramaga Static OK
+drmAvailable() = 0
+Pixman OK
+FreeType OK
+libpng 1.8.0.git
+Cairo 1.18.4
+Cairo OK
+[alb@zaramagaOS zlauncher]$ 
+
 $ZSYS/bin/musl-gcc     -static     pruebas.c     -I$ZSYS/include     -L$ZSYS/lib     -ldrm     -lpixman-1     -lfreetype     -lpng18     -lz     -lm     -o pruebas
 
 $ZSYS/bin/musl-gcc -static \
--- projects/zlauncher/pruebas.c
+++ projects/zlauncher/pruebas.c
@@ -2,7 +2,9 @@
 #include <pixman.h>
 #include <stdio.h>
 #include FT_FREETYPE_H
+#include <cairo/cairo.h>
 #include <png.h>
+#include <stdio.h>
 #include <xf86drm.h>
 
 int main(void) {
@@ -23,6 +25,21 @@ int main(void) {
 
   FT_Done_FreeType(ft);
   pixman_image_unref(img);
+  printf("Cairo %s\n", cairo_version_string());
+
+  cairo_surface_t *surface =
+      cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 100, 100);
+
+  cairo_t *cr = cairo_create(surface);
+
+  cairo_set_source_rgb(cr, 0, 1, 0);
+  cairo_paint(cr);
+
+  cairo_destroy(cr);
+  cairo_surface_destroy(surface);
+
+  puts("Cairo OK");
+  return 0;
 
   return 0;
 }
