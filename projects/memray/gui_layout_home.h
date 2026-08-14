655365420f890e7f366d4041a1bbc3edda3a7724
alb1122 <alb1122oli@gmail.com>
2026-08-14 16:42:36 +0200

    limpiando y adaptando

--- projects/memray/gui_layout_home.h
+++ projects/memray/gui_layout_home.h
@@ -34,12 +34,14 @@ typedef struct {
     // Define anchors
     Vector2 anchor01;            // ANCHOR ID:1
     Vector2 anchor02;            // ANCHOR ID:2
+    Vector2 anchor03;            // ANCHOR ID:3
+    Vector2 anchor04;            // ANCHOR ID:4
     
     // Define controls variables
     bool CheckBoxExAUTOBOOTChecked;            // CheckBoxEx: CheckBoxExAUTOBOOT
 
     // Define rectangles
-    Rectangle layoutRecs[21];
+    Rectangle layoutRecs[23];
 
     // Custom state variables (depend on development software)
     // NOTE: This variables should be added manually if required
@@ -72,6 +74,7 @@ static void ButtonKONSOLE();                // Button: ButtonKONSOLE logic
 static void ButtonWEBGAMESCOPE();                // Button: ButtonWEBGAMESCOPE logic
 static void ButtonTTY();                // Button: ButtonTTY logic
 static void ButtonOTHER();                // Button: ButtonOTHER logic
+static void ButtonPanelControl();                // Button: ButtonPanelControl logic
 
 #ifdef __cplusplus
 }
@@ -106,34 +109,38 @@ GuiLayoutHomeState InitGuiLayoutHome(void)
     GuiLayoutHomeState state = { 0 };
 
     // Init anchors
-    state.anchor01 = (Vector2){ 72, 144 };            // ANCHOR ID:1
-    state.anchor02 = (Vector2){ 792, 144 };            // ANCHOR ID:2
+    state.anchor01 = (Vector2){ 56, 64 };            // ANCHOR ID:1
+    state.anchor02 = (Vector2){ 992, 72 };            // ANCHOR ID:2
+    state.anchor03 = (Vector2){ 0, 0 };            // ANCHOR ID:3
+    state.anchor04 = (Vector2){ 216, 584 };            // ANCHOR ID:4
     
     // Initilize controls variables
     state.CheckBoxExAUTOBOOTChecked = false;            // CheckBoxEx: CheckBoxExAUTOBOOT
 
     // Init controls rectangles
-    state.layoutRecs[0] = (Rectangle){ 72, 48, 176, 24 };// Label: LabelKERNEL
-    state.layoutRecs[1] = (Rectangle){ 264, 48, 176, 24 };// Label: LabelCPU
-    state.layoutRecs[2] = (Rectangle){ 456, 48, 176, 24 };// Label: LabelRAM
-    state.layoutRecs[3] = (Rectangle){ 648, 48, 176, 24 };// Label: LabelGPU
-    state.layoutRecs[4] = (Rectangle){ 360, 216, 384, 240 };// DummyRec: DummyRecLOGO
-    state.layoutRecs[5] = (Rectangle){ 840, 48, 176, 24 };// Label: LabelDATEHOUR
-    state.layoutRecs[6] = (Rectangle){ 72, 600, 176, 24 };// Label: LabelAUDIO
-    state.layoutRecs[7] = (Rectangle){ 264, 600, 176, 24 };// Label: LabelSCREENS
-    state.layoutRecs[8] = (Rectangle){ 456, 600, 176, 24 };// Label: LabelNET
-    state.layoutRecs[9] = (Rectangle){ 648, 600, 176, 24 };// Label: LabelUSERS
-    state.layoutRecs[10] = (Rectangle){ 840, 600, 176, 24 };// Label: LabelAVANCED
-    state.layoutRecs[11] = (Rectangle){ state.anchor01.x + 0, state.anchor01.y + 0, 240, 408 };// GroupBox: GroupBox011
-    state.layoutRecs[12] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 24, 24, 24 };// CheckBoxEx: CheckBoxExAUTOBOOT
-    state.layoutRecs[13] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 72, 192, 24 };// Button: ButtonSWAYPRINCIPAL
-    state.layoutRecs[14] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 120, 192, 24 };// Button: ButtonSWAYGAMER
-    state.layoutRecs[15] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 168, 192, 24 };// Button: ButtonSTEAMD
-    state.layoutRecs[16] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 216, 192, 24 };// Button: ButtonKONSOLE
-    state.layoutRecs[17] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 264, 192, 24 };// Button: ButtonWEBGAMESCOPE
-    state.layoutRecs[18] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 312, 192, 24 };// Button: ButtonTTY
-    state.layoutRecs[19] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 360, 192, 24 };// Button: ButtonOTHER
-    state.layoutRecs[20] = (Rectangle){ state.anchor02.x + 0, state.anchor02.y + 0, 232, 408 };// GroupBox: GroupBox020
+    state.layoutRecs[0] = (Rectangle){ 56, 24, 176, 24 };// Label: LabelKERNEL
+    state.layoutRecs[1] = (Rectangle){ 336, 24, 176, 24 };// Label: LabelCPU
+    state.layoutRecs[2] = (Rectangle){ 712, 24, 176, 24 };// Label: LabelRAM
+    state.layoutRecs[3] = (Rectangle){ 992, 24, 176, 24 };// Label: LabelGPU
+    state.layoutRecs[4] = (Rectangle){ 504, 136, 280, 296 };// DummyRec: DummyRecLOGO
+    state.layoutRecs[5] = (Rectangle){ 600, 96, 176, 24 };// Label: LabelDATEHOUR
+    state.layoutRecs[6] = (Rectangle){ 56, 504, 176, 24 };// Label: LabelAUDIO
+    state.layoutRecs[7] = (Rectangle){ 288, 504, 176, 24 };// Label: LabelSCREENS
+    state.layoutRecs[8] = (Rectangle){ 680, 504, 176, 24 };// Label: LabelNET
+    state.layoutRecs[9] = (Rectangle){ 992, 504, 176, 24 };// Label: LabelUSERS
+    state.layoutRecs[10] = (Rectangle){ state.anchor01.x + 0, state.anchor01.y + 8, 240, 408 };// GroupBox: GroupBox011
+    state.layoutRecs[11] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 24, 24, 24 };// CheckBoxEx: CheckBoxExAUTOBOOT
+    state.layoutRecs[12] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 72, 192, 24 };// Button: ButtonSWAYPRINCIPAL
+    state.layoutRecs[13] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 120, 192, 24 };// Button: ButtonSWAYGAMER
+    state.layoutRecs[14] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 168, 192, 24 };// Button: ButtonSTEAMD
+    state.layoutRecs[15] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 216, 192, 24 };// Button: ButtonKONSOLE
+    state.layoutRecs[16] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 264, 192, 24 };// Button: ButtonWEBGAMESCOPE
+    state.layoutRecs[17] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 312, 192, 24 };// Button: ButtonTTY
+    state.layoutRecs[18] = (Rectangle){ state.anchor01.x + 24, state.anchor01.y + 360, 192, 24 };// Button: ButtonOTHER
+    state.layoutRecs[19] = (Rectangle){ state.anchor02.x + 0, state.anchor02.y + 0, 232, 408 };// GroupBox: GroupBox020
+    state.layoutRecs[20] = (Rectangle){ state.anchor03.x + 0, state.anchor03.y + 8, 1280, 752 };// GroupBox: GroupBoxMAIN
+    state.layoutRecs[21] = (Rectangle){ state.anchor04.x + 0, state.anchor04.y + 8, 784, 64 };// GroupBox: GroupBoxMETRICS
+    state.layoutRecs[22] = (Rectangle){ state.anchor03.x + 504, state.anchor03.y + 696, 280, 40 };// Button: ButtonPanelControl
 
     // Custom variables initialization
 
@@ -174,6 +181,11 @@ static void ButtonOTHER()
 {
     // TODO: Implement control logic
 }
+// Button: ButtonPanelControl logic
+static void ButtonPanelControl()
+{
+    // TODO: Implement control logic
+}
 
 
 void GuiLayoutHome(GuiLayoutHomeState *state)
@@ -189,7 +201,6 @@ void GuiLayoutHome(GuiLayoutHomeState *state)
     const char *LabelSCREENSText = "SCREENS";    // LABEL: LabelSCREENS
     const char *LabelNETText = "NET";    // LABEL: LabelNET
     const char *LabelUSERSText = "USERS";    // LABEL: LabelUSERS
-    const char *LabelAVANCEDText = "AVANCED";    // LABEL: LabelAVANCED
     const char *GroupBox011Text = "LAUNCHER";    // GROUPBOX: GroupBox011
     const char *CheckBoxExAUTOBOOTText = "AUTO LAUNCH ON BOOT";    // CHECKBOXEX: CheckBoxExAUTOBOOT
     const char *ButtonSWAYPRINCIPALText = "SWAY PRINCIPAL";    // BUTTON: ButtonSWAYPRINCIPAL
@@ -200,6 +211,9 @@ void GuiLayoutHome(GuiLayoutHomeState *state)
     const char *ButtonTTYText = "TTY";    // BUTTON: ButtonTTY
     const char *ButtonOTHERText = "OTHERS";    // BUTTON: ButtonOTHER
     const char *GroupBox020Text = "SYSTEM";    // GROUPBOX: GroupBox020
+    const char *GroupBoxMAINText = "ZLAUNCHER";    // GROUPBOX: GroupBoxMAIN
+    const char *GroupBoxMETRICSText = "METRICS";    // GROUPBOX: GroupBoxMETRICS
+    const char *ButtonPanelControlText = "CONTROL PANEL";    // BUTTON: ButtonPanelControl
     
     // Draw controls
     GuiLabel(state->layoutRecs[0], LabelKERNELText);
@@ -212,17 +226,19 @@ void GuiLayoutHome(GuiLayoutHomeState *state)
     GuiLabel(state->layoutRecs[7], LabelSCREENSText);
     GuiLabel(state->layoutRecs[8], LabelNETText);
     GuiLabel(state->layoutRecs[9], LabelUSERSText);
-    GuiLabel(state->layoutRecs[10], LabelAVANCEDText);
-    GuiGroupBox(state->layoutRecs[11], GroupBox011Text);
-    GuiCheckBox(state->layoutRecs[12], CheckBoxExAUTOBOOTText, &state->CheckBoxExAUTOBOOTChecked);
-    if (GuiButton(state->layoutRecs[13], ButtonSWAYPRINCIPALText)) ButtonSWAYPRINCIPAL(); 
-    if (GuiButton(state->layoutRecs[14], ButtonSWAYGAMERText)) ButtonSWAYGAMER(); 
-    if (GuiButton(state->layoutRecs[15], ButtonSTEAMDText)) ButtonSTEAMD(); 
-    if (GuiButton(state->layoutRecs[16], ButtonKONSOLEText)) ButtonKONSOLE(); 
-    if (GuiButton(state->layoutRecs[17], ButtonWEBGAMESCOPEText)) ButtonWEBGAMESCOPE(); 
-    if (GuiButton(state->layoutRecs[18], ButtonTTYText)) ButtonTTY(); 
-    if (GuiButton(state->layoutRecs[19], ButtonOTHERText)) ButtonOTHER(); 
-    GuiGroupBox(state->layoutRecs[20], GroupBox020Text);
+    GuiGroupBox(state->layoutRecs[10], GroupBox011Text);
+    GuiCheckBox(state->layoutRecs[11], CheckBoxExAUTOBOOTText, &state->CheckBoxExAUTOBOOTChecked);
+    if (GuiButton(state->layoutRecs[12], ButtonSWAYPRINCIPALText)) ButtonSWAYPRINCIPAL(); 
+    if (GuiButton(state->layoutRecs[13], ButtonSWAYGAMERText)) ButtonSWAYGAMER(); 
+    if (GuiButton(state->layoutRecs[14], ButtonSTEAMDText)) ButtonSTEAMD(); 
+    if (GuiButton(state->layoutRecs[15], ButtonKONSOLEText)) ButtonKONSOLE(); 
+    if (GuiButton(state->layoutRecs[16], ButtonWEBGAMESCOPEText)) ButtonWEBGAMESCOPE(); 
+    if (GuiButton(state->layoutRecs[17], ButtonTTYText)) ButtonTTY(); 
+    if (GuiButton(state->layoutRecs[18], ButtonOTHERText)) ButtonOTHER(); 
+    GuiGroupBox(state->layoutRecs[19], GroupBox020Text);
+    GuiGroupBox(state->layoutRecs[20], GroupBoxMAINText);
+    GuiGroupBox(state->layoutRecs[21], GroupBoxMETRICSText);
+    if (GuiButton(state->layoutRecs[22], ButtonPanelControlText)) ButtonPanelControl(); 
 }
 
 #endif // GUI_LAYOUT_HOME_IMPLEMENTATION
---
+++ projects/memray/wayClean.c
@@ -0,0 +1,638 @@
+#include "raylib.h"
+#include "rlgl.h"
+#include "xdg-shell.h"
+#include <wayland-client.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+ #define GUI_LAYOUT_HOME_IMPLEMENTATION
+ #include "gui_layout_home.h"
+
+#define WIDTH 1280
+#define HEIGHT 768
+#define SIZE (WIDTH * HEIGHT * 2)
+static bool need_redraw = true;
+static bool isHover = false;
+static bool oldHover = false;
+unsigned char *pixels = NULL;
+static struct wl_display *display;
+static struct wl_registry *registry;
+static struct wl_compositor *compositor;
+static struct wl_shm *shm;
+// Framebuffer permanente en BSS
+// static unsigned char g_framebuffer[WIDTH * HEIGHT * 2];
+
+// // Puntero constante al framebuffer
+// unsigned char *const g_pBuffer = g_framebuffer;
+
+// static unsigned char framebuffer[SIZE];
+
+static struct wl_display *display;
+static struct wl_compositor *compositor;
+static struct wl_shm *shm;
+static struct wl_surface *surface;
+static struct wl_shm_pool *pool;
+static struct wl_buffer *buffer;
+struct wl_seat *seat = NULL;
+struct wl_pointer *pointer = NULL;
+
+static struct xdg_wm_base *xdg_wm_base;
+static struct xdg_surface *xdg_surface;
+static struct xdg_toplevel *xdg_toplevel;
+static int configured = 0;
+
+static float g_mouse_x = 0.0f;
+static float g_mouse_y = 0.0f;
+static unsigned long ray_frame_count = 0;
+static unsigned long way_frame_count = 0;
+typedef struct
+{
+    float mouse_x;
+    float mouse_y;
+    bool mouse_inside;      // Indica si el ratón está dentro de la ventana
+    bool btn_left_pressed;  // Clic izquierdo
+    bool btn_right_pressed; // Clic derecho
+    float scroll_y;         // Desplazamiento de la rueda del ratón
+} WaylandInputState;
+static WaylandInputState g_input = {0};
+static void pointer_handle_frame(
+    void *data,
+    struct wl_pointer *pointer)
+{
+    /* Fin del grupo de eventos del puntero */
+}
+static void pointer_handle_enter(void *data, struct wl_pointer *pointer,
+                                 uint32_t serial, struct wl_surface *surface,
+                                 wl_fixed_t sx, wl_fixed_t sy)
+{
+    g_input.mouse_inside = true;
+    g_input.mouse_x = (float)wl_fixed_to_double(sx);
+    g_input.mouse_y = (float)wl_fixed_to_double(sy);
+    printf("Mouse entered at: (%.2f, %.2f)\n", g_input.mouse_x, g_input.mouse_y);
+    // Opcional: Cambiar el cursor a uno personalizado o del sistema aquí
+}
+// 2. Salir de la ventana (Pierde el foco el puntero)
+static void pointer_handle_leave(void *data, struct wl_pointer *pointer,
+                                 uint32_t serial, struct wl_surface *surface)
+{
+    g_input.mouse_inside = false;
+    printf("Mouse left the window\n");
+    // Reseteamos estados de botones al salir por seguridad
+    g_input.btn_left_pressed = false;
+    g_input.btn_right_pressed = false;
+}
+static void pointer_handle_button(void *data, struct wl_pointer *pointer,
+                                  uint32_t serial, uint32_t time,
+                                  uint32_t button, uint32_t state)
+{
+    // Linux linux/input-event-codes.h: BTN_LEFT = 272 (0x110), BTN_RIGHT = 273 (0x111)
+    bool is_pressed = (state == WL_POINTER_BUTTON_STATE_PRESSED);
+
+    if (button == 272)
+    { // BTN_LEFT
+        g_input.btn_left_pressed = is_pressed;
+    }
+    else if (button == 273)
+    { // BTN_RIGHT
+        g_input.btn_right_pressed = is_pressed;
+    }
+}
+static void pointer_handle_axis(void *data, struct wl_pointer *pointer,
+                                uint32_t time, uint32_t axis, wl_fixed_t value)
+{
+    // axis == WL_POINTER_AXIS_VERTICAL_SCROLL (0)
+    if (axis == WL_POINTER_AXIS_VERTICAL_SCROLL)
+    {
+        // En Wayland el valor viene en escala de subpíxeles, lo normalizamos
+        g_input.scroll_y += (float)wl_fixed_to_double(value);
+    }
+}
+// Callback de movimiento del puntero de Wayland
+static void pointer_handle_motion(void *data, struct wl_pointer *pointer,
+                                  uint32_t time, wl_fixed_t sx, wl_fixed_t sy)
+{
+    // Convertimos de formato punto fijo (wl_fixed_t) a float
+    g_input.mouse_x = wl_fixed_to_double(sx);
+    g_input.mouse_y = wl_fixed_to_double(sy);
+    printf("Mouse moved to: (%.2f, %.2f)\n", g_input.mouse_x, g_input.mouse_y);
+}
+static const struct wl_pointer_listener pointer_listener = {
+    .enter = pointer_handle_enter,
+    .leave = pointer_handle_leave,
+    .motion = pointer_handle_motion, // <--- Este evento actualiza la posición
+    .button = pointer_handle_button,
+    .axis = pointer_handle_axis,
+    .frame = pointer_handle_frame,
+};
+
+static void xdg_ping(
+    void *data,
+    struct xdg_wm_base *xdg_wm_base,
+    uint32_t serial)
+{
+    xdg_wm_base_pong(xdg_wm_base, serial);
+}
+
+static void xdg_surface_configure(
+    void *data,
+    struct xdg_surface *xdg_surface,
+    uint32_t serial)
+{
+    xdg_surface_ack_configure(
+        xdg_surface,
+        serial);
+
+    configured = 1;
+}
+
+static const struct xdg_wm_base_listener xdg_wm_base_listener = {
+    .ping = xdg_ping};
+
+static const struct xdg_surface_listener xdg_surface_listener = {
+    .configure = xdg_surface_configure};
+static void registry_global(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t name,
+    const char *interface,
+    uint32_t version)
+{
+    if (strcmp(interface, "wl_compositor") == 0)
+    {
+        compositor = wl_registry_bind(
+            registry,
+            name,
+            &wl_compositor_interface,
+            4);
+    }
+    else if (strcmp(interface, "wl_shm") == 0)
+    {
+        shm = wl_registry_bind(
+            registry,
+            name,
+            &wl_shm_interface,
+            1);
+    }
+    else if (strcmp(interface, "xdg_wm_base") == 0)
+    {
+        xdg_wm_base = wl_registry_bind(
+            registry,
+            name,
+            &xdg_wm_base_interface,
+            1);
+    }
+    else if (strcmp(interface, wl_seat_interface.name) == 0)
+    {
+        uint32_t seat_version = version < 5 ? version : 5;
+        seat = wl_registry_bind(registry, name, &wl_seat_interface, seat_version);
+        pointer = wl_seat_get_pointer(seat);
+
+        // Asignamos nuestra listener de callbacks
+        // wl_pointer_add_listener(pointer, &pointer_listener, &g_input);
+        if (pointer)
+        {
+            // Asignamos la listener de callbacks del ratón
+            wl_pointer_add_listener(pointer, &pointer_listener, &g_input);
+        }
+    }
+}
+
+static void registry_remove(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t name)
+{
+}
+
+static const struct wl_registry_listener registry_listener = {
+    registry_global,
+    registry_remove};
+int wayInit()
+{
+    display = wl_display_connect(NULL);
+
+    if (!display)
+    {
+        fprintf(stderr, "No se pudo conectar a Wayland\n");
+        return 1;
+    }
+
+    /* -----------------------------------------
+     * Obtener globals
+     * ----------------------------------------- */
+
+    registry = wl_display_get_registry(display);
+
+    wl_registry_add_listener(
+        registry,
+        &registry_listener,
+        NULL);
+
+    wl_display_roundtrip(display);
+
+    xdg_wm_base_add_listener(
+        xdg_wm_base,
+        &xdg_wm_base_listener,
+        NULL);
+
+    if (!compositor || !shm || !xdg_wm_base)
+    {
+        fprintf(stderr,
+                "Faltan compositor, SHM o xdg_wm_base\n");
+        return 1;
+    }
+    surface =
+        wl_compositor_create_surface(compositor);
+
+    if (!surface)
+    {
+        fprintf(stderr,
+                "No se pudo crear wl_surface\n");
+        return 1;
+    }
+
+    /* -----------------------------------------
+     * Crear xdg_surface
+     * ----------------------------------------- */
+
+    xdg_surface =
+        xdg_wm_base_get_xdg_surface(
+            xdg_wm_base,
+            surface);
+
+    if (!xdg_surface)
+    {
+        fprintf(stderr,
+                "No se pudo crear xdg_surface\n");
+        return 1;
+    }
+
+    xdg_surface_add_listener(
+        xdg_surface,
+        &xdg_surface_listener,
+        NULL);
+
+    /* -----------------------------------------
+     * Crear ventana toplevel
+     * ----------------------------------------- */
+
+    xdg_toplevel =
+        xdg_surface_get_toplevel(
+            xdg_surface);
+
+    if (!xdg_toplevel)
+    {
+        fprintf(stderr,
+                "No se pudo crear xdg_toplevel\n");
+        return 1;
+    }
+
+    xdg_toplevel_set_title(
+        xdg_toplevel,
+        "WAYMIX");
+    /* -----------------------------------------
+     * Primer commit SIN BUFFER
+     * ----------------------------------------- */
+
+    wl_surface_commit(surface);
+
+    /* -----------------------------------------
+     * Esperar configure inicial
+     * ----------------------------------------- */
+
+    wl_display_roundtrip(display);
+
+    if (!configured)
+    {
+        fprintf(stderr,
+                "No recibimos el configure inicial\n");
+        return 1;
+    }
+
+    printf("Configure recibido\n");
+
+    /* -----------------------------------------
+     * Crear SHM
+     * ----------------------------------------- */
+
+    int fd =
+        shm_open(
+            "/ztest-wayland",
+            O_CREAT | O_RDWR,
+            0600);
+
+    if (fd < 0)
+    {
+        perror("shm_open");
+        return 1;
+    }
+
+    shm_unlink("/ztest-wayland");
+
+    if (ftruncate(fd, SIZE) < 0)
+    {
+        perror("ftruncate");
+        close(fd);
+        return 1;
+    }
+
+    /* -----------------------------------------
+     * Mapear memoria
+     * ----------------------------------------- */
+
+    pixels =
+        mmap(
+            NULL,
+            SIZE,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            0);
+
+    if (pixels == MAP_FAILED)
+    {
+        perror("mmap");
+        close(fd);
+        return 1;
+    }
+
+    /* -----------------------------------------
+     * Limpiar framebuffer
+     * ----------------------------------------- */
+
+    memset(
+        pixels,
+        0,
+        SIZE);
+
+    /* -----------------------------------------
+     * Crear pool Wayland
+     * ----------------------------------------- */
+
+    pool =
+        wl_shm_create_pool(
+            shm,
+            fd,
+            SIZE);
+
+    if (!pool)
+    {
+        fprintf(stderr,
+                "No se pudo crear wl_shm_pool\n");
+        return 1;
+    }
+
+    /* -----------------------------------------
+     * Crear wl_buffer
+     * ----------------------------------------- */
+
+    buffer =
+        wl_shm_pool_create_buffer(
+            pool,
+            0,
+            WIDTH,
+            HEIGHT,
+            WIDTH * 2,
+            // WL_SHM_FORMAT_XRGB8888);
+            WL_SHM_FORMAT_RGBA4444);
+    if (!buffer)
+    {
+        fprintf(stderr,
+                "No se pudo crear wl_buffer\n");
+        return 1;
+    }
+    return fd;
+}
+int limpieza(int fd)
+{
+    /* -----------------------------------------
+     * Limpieza
+     * ----------------------------------------- */
+    // UnloadRenderTexture(target);
+    wl_buffer_destroy(buffer);
+    wl_shm_pool_destroy(pool);
+
+    munmap(
+        pixels,
+        SIZE);
+
+    close(fd);
+
+    if (pointer)
+        wl_pointer_destroy(pointer);
+
+    if (seat)
+        wl_seat_destroy(seat);
+
+    xdg_toplevel_destroy(xdg_toplevel);
+    xdg_surface_destroy(xdg_surface);
+    wl_surface_destroy(surface);
+
+    if (xdg_wm_base)
+        xdg_wm_base_destroy(xdg_wm_base);
+
+    if (shm)
+        wl_shm_destroy(shm);
+
+    if (compositor)
+        wl_compositor_destroy(compositor);
+
+    wl_registry_destroy(registry);
+
+    wl_display_disconnect(display);
+
+    CloseWindow();
+}
+int rayInit()
+{
+    /* -----------------------------------------
+     * Inicializar Raylib
+     * ----------------------------------------- */
+
+    InitWindow(
+        WIDTH,
+        HEIGHT,
+        "WAYMIX3");
+    // RenderTexture2D target = LoadRenderTexture(800, 600);
+    // SetTextureFilter(target.texture, TEXTURE_FILTER_TRILINEAR);
+    SetTargetFPS(10);
+    GuiLayoutHomeState layout = InitGuiLayoutHome();
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 18);
+    
+    /* -----------------------------------------
+     * Posición inicial del ratón
+     * ----------------------------------------- */
+
+    SetMousePosition(
+        WIDTH / 2,
+        HEIGHT / 2);
+
+    while (!WindowShouldClose())
+    {
+        wl_display_dispatch_pending(display);
+
+        /*
+         * -----------------------------------------
+         * Procesar input
+         * -----------------------------------------
+         */
+
+        SetMousePosition(
+            (int)g_input.mouse_x,
+            (int)g_input.mouse_y);
+
+        Vector2 mouse = GetMousePosition();
+
+        isHover =
+            CheckCollisionPointRec(
+                mouse,
+                layout.layoutRecs[13]);
+        if (isHover != oldHover)
+        {
+            oldHover = isHover;
+            need_redraw = true;
+
+            printf(
+                "HOVER CAMBIO -> %d\n",
+                isHover);
+        }
+
+        printf(
+            "mouse Ray= %.0f %.0f | hover Ray= %d\n",
+            mouse.x,
+            mouse.y,
+            isHover);
+
+        /*
+         * -----------------------------------------
+         * RENDER
+         * -----------------------------------------
+         */
+
+        if (need_redraw)
+        {
+            ray_frame_count++;
+
+            printf("ray framecount %d\n", ray_frame_count);
+
+            BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            Vector2 mouse = GetMousePosition();
+
+            bool inside =
+                mouse.x >= 300 &&
+                mouse.x <= 500 &&
+                mouse.y >= 450 &&
+                mouse.y <= 530;
+
+            DrawText(
+                TextFormat(
+                    "RAYLIB MOUSE: %.1f %.1f",
+                    mouse.x,
+                    mouse.y),
+                20,
+                20,
+                25,
+                GREEN);
+
+            DrawText(
+                inside ? "HOVER: SI" : "HOVER: NO",
+                20,
+                55,
+                25,
+                inside ? GREEN : GRAY);
+
+            GuiLayoutHome(&layout);
+
+            EndDrawing();
+
+            /*
+             * -----------------------------------------
+             * Raylib → SHM
+             * -----------------------------------------
+             */
+
+            rlCopyFramebuffer(
+                0,
+                0,
+                WIDTH,
+                HEIGHT,
+                // RL_PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+                RL_PIXELFORMAT_UNCOMPRESSED_R4G4B4A4,
+                pixels);
+
+            /*
+             * -----------------------------------------
+             * Wayland PRESENT
+             * -----------------------------------------
+             */
+
+            wl_surface_attach(
+                surface,
+                buffer,
+                0,
+                0);
+
+            wl_surface_damage_buffer(
+                surface,
+                0,
+                0,
+                WIDTH,
+                HEIGHT);
+
+            wl_surface_commit(surface);
+
+            wl_display_flush(display);
+
+            way_frame_count++;
+
+            printf(
+                "way frames %d\n",
+                way_frame_count);
+
+            /*
+             * -----------------------------------------
+             * Ya hemos dibujado este estado
+             * -----------------------------------------
+             */
+
+            need_redraw = false;
+        }
+
+        /*
+         * -----------------------------------------
+         * Eventos Wayland
+         * -----------------------------------------
+         */
+
+        if (wl_display_dispatch(display) < 0)
+            break;
+    }
+    return 0;
+}
+int main(void)
+{
+    printf("Wayland %dx%d\n", WIDTH, HEIGHT);
+    int fd = wayInit();
+    int ok = rayInit();
+    if (ok==0)
+    {
+       printf("RAYINIT CORRECTO");  
+    }else
+    {
+        return 1;
+    }
+      
+    limpieza(fd);
+    return 0;
+}
